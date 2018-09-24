91d674d30983a704b8354cd48f4b2ffd833f9b4b
Martin Avagyan <martinavagyan@Martins-MacBook-Pro-2.local>
2018-09-24 16:30:43 +0200

    Add NFC tag reader

--- app/src/main/java/com/ingwbaa/martinavagyan/stairoid_app/MainActivity.java
+++ app/src/main/java/com/ingwbaa/martinavagyan/stairoid_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.ingwbaa.martinavagyan.stairoid_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ingwbaa.martinavagyan.stairoid_app.nfc_reader.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/ingwbaa/martinavagyan/stairoid_app/nfc_reader/record/ParsedNdefRecord.java
@@ -0,0 +1,18 @@
+
+package com.ingwbaa.martinavagyan.stairoid_app.nfc_reader.record;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public interface ParsedNdefRecord {
+
+    /**
+     * Returns a view to display this record.
+     */
+    public View getView(Activity activity, LayoutInflater inflater, ViewGroup parent,
+            int offset);
+
+}
